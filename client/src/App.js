ec22c0944f593158140e066d69f328cd90fb3f3a
Kyu Sung Park <qkrrbtjd90@gmail.com>
2024-03-24 02:16:59 -0400

    add basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import AboutPage from './AboutPage';
+import HomePage from './HomePage';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomePage />,
+	},
+	{
+		path: '/about',
+		element: <AboutPage />,
+	},
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <RouterProvider router={router} />;
 }
 
 export default App;
