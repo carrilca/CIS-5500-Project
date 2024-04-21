f2b8dac52fb32a1eb3ab766402ae1110f7410a4a
sregmi <regmishashwat@gmail.com>
2024-04-20 20:40:51 -0400

    Deployment URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:8080',
+	baseURL: 'https://cis-5500-project.onrender.com',
 	headers: {
 		Accept: 'application/json',
 		'Content-Type': 'application/json',
